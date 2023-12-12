b93913a7f5b3dcb8ce7e8319876de3d0485644dc
iseruuuuu <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>
2023-12-12 09:28:26 +0900

    add: create splash screen

---
+++ lib/ui/screen/splash/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:supabase_hackthon_app/router/router.dart';
+import 'package:supabase_hackthon_app/ui/component/app_loading.dart';
+import 'package:supabase_hackthon_app/utils/mixin/account_exist_mixin.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> with AccountExistMixin {
+  @override
+  void initState() {
+    redirect(context);
+    super.initState();
+  }
+
+  Future<void> redirect(BuildContext context) async {
+    await Future.delayed(Duration.zero);
+    if (!await doesAccountExist()) {
+      context.pushReplacementNamed(RouterPath.newAccount);
+    } else {
+      context.pushReplacementNamed(RouterPath.tab);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: AppLoading(loading: true),
+      ),
+    );
+  }
+}
