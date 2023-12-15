092376956af6d09ad945504d7de25be03c5111be
iseruuuuu <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>
2023-12-15 10:00:36 +0900

    fix: set image imageQuality

--- lib/ui/screen/post/post_view_model.dart
+++ lib/ui/screen/post/post_view_model.dart
@@ -68,7 +68,12 @@ class PostViewModel extends _$PostViewModel {
 
   Future<void> camera() async {
     try {
-      final image = await ImagePicker().pickImage(source: ImageSource.camera);
+      final image = await ImagePicker().pickImage(
+        source: ImageSource.camera,
+        maxHeight: 960,
+        maxWidth: 960,
+        imageQuality: 10,
+      );
       if (image == null) {
         return;
       }
@@ -86,7 +91,12 @@ class PostViewModel extends _$PostViewModel {
 
   Future<void> album() async {
     try {
-      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      final image = await ImagePicker().pickImage(
+        source: ImageSource.gallery,
+        maxHeight: 960,
+        maxWidth: 960,
+        imageQuality: 10,
+      );
       if (image == null) {
         return;
       }
